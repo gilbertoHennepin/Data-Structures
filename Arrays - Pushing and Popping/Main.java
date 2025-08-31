fa8cb2b4b59b0a6e87ed78c04891bb71b437c8ec
gilbertoHennepin <155484528+gilbertoHennepin@users.noreply.github.com>
2025-08-30 23:23:10 -0500

    Update Main.java

--- Arrays - Pushing and Popping/Main.java
+++ Arrays - Pushing and Popping/Main.java
@@ -3,7 +3,6 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 
-
 public class Main {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
