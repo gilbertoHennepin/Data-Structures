9b5ee83d11cccc9488f1a6d2bc33b8de4cdc1f3d
gilbertoHennepin <155484528+gilbertoHennepin@users.noreply.github.com>
2025-09-03 23:58:52 -0500

    Update Main.java

--- Arrays - Pushing and Popping/Main.java
+++ Arrays - Pushing and Popping/Main.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 public class Main {
 
 
-    
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
         TextEditorUndo undoStack = new TextEditorUndo(10); // Maximum 10 actions
